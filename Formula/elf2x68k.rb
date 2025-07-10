f71885ecad2191f704d50c71a8bec9e326e3480f
Yuichi Nakamura <y.512.nakamura@gmail.com>
2025-07-11 01:08:34 +0900

    Remove m68k-gdb and update elf2x68k version to 20250712

--- Formula/elf2x68k.rb
+++ Formula/elf2x68k.rb
@@ -2,16 +2,18 @@ class Elf2x68k < Formula
   desc "X68k cross development environment"
   homepage "https://github.com/yunkya2/elf2x68k"
   url "https://github.com/yunkya2/elf2x68k.git",
-     tag: "20250410"
+     tag: "20250712"
 
   depends_on "wget"
   depends_on "texinfo"
+  conflicts_with "m68k-gdb", because: "elf2x68k includes `m68k-elf-gdb` binary"
 
   def install
     system "make", "binutils", "INSTALL_DIR=#{prefix}"
     system "make", "gcc-stage1", "INSTALL_DIR=#{prefix}"
     system "make", "newlib", "INSTALL_DIR=#{prefix}"
     system "make", "gcc-stage2", "INSTALL_DIR=#{prefix}"
+    system "make", "gdb", "INSTALL_DIR=#{prefix}"
     system "make", "install", "INSTALL_DIR=#{prefix}"
     system "bash", "-c", "(cd #{prefix};./install-xclib.sh)"
   end
