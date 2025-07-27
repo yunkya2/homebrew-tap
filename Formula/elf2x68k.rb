5779f7f05406d03a035dc76b721225286d23bf8f
Yuichi Nakamura <y.512.nakamura@gmail.com>
2025-07-27 09:37:15 +0900

    Update elf2x68k version to 20250727

--- Formula/elf2x68k.rb
+++ Formula/elf2x68k.rb
@@ -2,11 +2,10 @@ class Elf2x68k < Formula
   desc "X68k cross development environment"
   homepage "https://github.com/yunkya2/elf2x68k"
   url "https://github.com/yunkya2/elf2x68k.git",
-     tag: "20250726"
+     tag: "20250727"
 
   depends_on "wget"
   depends_on "texinfo"
-  conflicts_with "m68k-gdb", because: "elf2x68k includes `m68k-elf-gdb` binary"
 
   def install
     system "make", "binutils", "INSTALL_DIR=#{prefix}"
@@ -14,6 +13,7 @@ class Elf2x68k < Formula
     system "make", "newlib", "INSTALL_DIR=#{prefix}"
     system "make", "gcc-stage2", "INSTALL_DIR=#{prefix}"
     system "make", "gdb", "INSTALL_DIR=#{prefix}"
+    ENV.deparallelize
     system "make", "install", "INSTALL_DIR=#{prefix}"
     system "bash", "-c", "(cd #{prefix};./install-xclib.sh)"
   end
